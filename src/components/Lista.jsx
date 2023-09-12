23c69e4b9ec439ead69b6bd410aacd24a2244bd1
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-09-12 09:13:46 -0300

    Estilizando tablea com css..module

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -8,15 +8,19 @@ export default function Lista() {
         <li>
           <Link to="/">Home</Link>
         </li>
+
         <li>
         <Link to="/produtos">Produtos</Link>
         </li>
+
         <li>
         <Link to="/editar/produtos/1">Produto - 1</Link>
         </li>
+
         <li>
         <Link to="/editar/produtos/2">Produto - 2</Link>
         </li>
+        
         <li>
         <Link to="/editar/produtos/3">Produto - 3</Link>
         </li>
