f73dee63fd4eecca542733da9e2eace050116c5f
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-09-21 09:43:47 -0300

    ajuste cd=ss img produto. adicionado arquivo json

--- src/routes/AdicionarProduto.jsx
+++ src/routes/AdicionarProduto.jsx
@@ -1,59 +1,69 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
+import {ListaProdutos} from "../components/ListaProdutos";
+import { useNavigate } from "react-router-dom";
 
 export default function AdicionarProduto() {
-
-    document.title = "Criar Produtos";
-
+    document.title = 'Inserir Produto';
     const navigate = useNavigate();
 
-    const {id} = useParams(); 
-    let contadorID = ListaProdutos.length
-    
-    let produtoNovo = ListaProdutos.filter( produto => produto.id == (contadorID+1) );
-    
-    const [produtoId, setProdutoId] = useState(0)
+    // const [produtoId, setProdutoId] = useState(0);
+    let idProd = parseInt(ListaProdutos[ListaProdutos.length-1].id) + 1;
     const [produto, setProduto] = useState({
-        id:0,
+        id:idProd,
         nome:"",
         desc:"",
-        preco:"",
-        img:"",
-    })
+        preco:0.0,
+        img:"" 
+    });
+    
+    const handleChange = (e)=>{
+        e.preventDefault();
+        const {name,value} = e.target;
+        setProduto({...produto,[name]:value});
+    }
 
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        
+        // setProdutoId(idProd);
+        // const{id} = produto;
+        // setProduto({...produto,id:produtoId})
 
+        ListaProdutos.push(produto);
 
-    return (
+        alert("Produto cadastrado com sucesso...");
+        navigate("/produtos")
+    };
 
-<div>
-    <h1>Adicionar produto</h1>
+  return (
     <div>
-        <form action="">
-            <fieldset>
-                <legend>Cadastrar Produto</legend>
-                <div>
-                    <label htmlFor="idNome">Nome: </label>
-                    <input type="text" name='nome' id='idNome' value={produto.nome} />
-                </div>
-                <div>
-                    <label htmlFor="idDesc">Descrição: </label>
-                    <input type="number" name='desc' id='idDesc' value={produto.desc} />
-                </div>
-                <div>
-                    <label htmlFor="idPreco">Preço: </label>
-                    <input type="text" name='preco' id='idpreco' value={produto.preco} />
-                </div>
-                <div>
-                    <label htmlFor="idImg">Imagem: </label>
-                    <input type="text" name='imagem' id='idImg' value={produto.img} />
-                </div>
-            </fieldset>
-        </form>
-
+        <h1>Inserir Produto</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>Cadastrar Produto</legend>
+                    <div>
+                        <label htmlFor="idNome">Nome:</label>
+                        <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="idDesc">Descrição:</label>
+                        <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="idPreco">Preço:</label>
+                        <input type="number" name="preco" id="idPreco" value={produto.preco} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="idImg">Imagem:</label>
+                        <input type="url" name="img" id="idImg" value={produto.img} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <button>Cadastrar</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
     </div>
-</div>
-
-
-    )
+  )
 }
